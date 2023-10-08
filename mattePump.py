0881a57861959bb35c23caaeaaddd65af66d7ff6
ecle20yy <ecle20yy@student.ju.se>
2023-10-08 21:53:22 +0200

    Update comments

--- mattePump.py
+++ mattePump.py
@@ -1,6 +1,7 @@
 import random
 
 # Generate a two value positive intiger addition question
+# Largest sum
 def generateAdditionQuestion(minTotalValue: int, maxTotalValue: int):
     if minTotalValue < 2:
         raise ValueError("Minimum value == 2")
@@ -11,6 +12,7 @@ def generateAdditionQuestion(minTotalValue: int, maxTotalValue: int):
     return str(firstValue) + " + " + str(secondValue) + " = __" + str(totalValue)
 
 # Generate a two value positive intiger subtraction question
+# Largest value subtracted
 def generateSubtractionQuestion(minSubtractedTerm: int, maxSubtractedTerm: int):
     firstTerm = random.randint(minSubtractedTerm, maxSubtractedTerm)
     secondTerm = random.randint(1, firstTerm)
