569fe9a8154341302c6da1e7d5747e2d44c05bfb
ecle20yy <ecle20yy@student.ju.se>
2023-10-08 21:51:42 +0200

    Add subtraction question generation

--- mattePump.py
+++ mattePump.py
@@ -1,6 +1,6 @@
 import random
 
-# Generate a two value question. 
+# Generate a two value positive intiger addition question
 def generateAdditionQuestion(minTotalValue: int, maxTotalValue: int):
     if minTotalValue < 2:
         raise ValueError("Minimum value == 2")
@@ -10,4 +10,11 @@ def generateAdditionQuestion(minTotalValue: int, maxTotalValue: int):
 
     return str(firstValue) + " + " + str(secondValue) + " = __" + str(totalValue)
 
-print (generateAdditionQuestion(2, 10))
+# Generate a two value positive intiger subtraction question
+def generateSubtractionQuestion(minSubtractedTerm: int, maxSubtractedTerm: int):
+    firstTerm = random.randint(minSubtractedTerm, maxSubtractedTerm)
+    secondTerm = random.randint(1, firstTerm)
+
+    return str(firstTerm) + " - " + str(secondTerm) + " = __"
+
+print (generateSubtractionQuestion(2, 10))
