40efd075d2f0b97b3fdb9eb8455f0398f63e1915
ecle20yy <ecle20yy@student.ju.se>
2023-10-08 21:42:51 +0200

    reformat question generator

--- mattePump.py
+++ mattePump.py
@@ -1,6 +1,13 @@
 import random
 
-def generateQuestion():
-    return str(random.randint(0, 10)) + " + " + str(random.randint(0, 10)) + " = __"
+# Generate a two value question. 
+def generateAdditionQuestion(minTotalValue: int, maxTotalValue: int):
+    if minTotalValue < 2:
+        raise ValueError("Minimum value == 2")
+    totalValue = random.randint(minTotalValue, maxTotalValue)
+    firstValue = totalValue - random.randint(1, totalValue-1)
+    secondValue = totalValue - firstValue
 
-print (generateQuestion())
+    return str(firstValue) + " + " + str(secondValue) + " = __" + str(totalValue)
+
+print (generateAdditionQuestion(2, 10))
