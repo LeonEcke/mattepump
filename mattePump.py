b71309355d12fef2fbdf358d1b62a99e137b7dc1
ecle20yy <ecle20yy@student.ju.se>
2023-10-08 22:29:50 +0200

    Add pdf output

--- mattePump.py
+++ mattePump.py
@@ -1,4 +1,8 @@
 import random
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+from datetime import date
 
 # Generate a two value positive intiger addition question
 # Largest sum
@@ -9,7 +13,7 @@ def generateAdditionQuestion(minTotalValue: int, maxTotalValue: int):
     firstValue = totalValue - random.randint(1, totalValue-1)
     secondValue = totalValue - firstValue
 
-    return str(firstValue) + " + " + str(secondValue) + " = __" + str(totalValue)
+    return str(firstValue) + " + " + str(secondValue) + " = __"
 
 # Generate a two value positive intiger subtraction question
 # Largest value subtracted
@@ -19,4 +23,19 @@ def generateSubtractionQuestion(minSubtractedTerm: int, maxSubtractedTerm: int):
 
     return str(firstTerm) + " - " + str(secondTerm) + " = __"
 
-print (generateSubtractionQuestion(2, 10))
+def generateQuestionSheet():
+    c = canvas.Canvas("MattePump.pdf", pagesize=A4)
+    c.setFillColor(colors.black)
+    c.setFont("Helvetica", 24)
+    for ix in range(0, 14):
+        c.drawString(70, ix*50+100, generateAdditionQuestion(2, 10))
+    for ix in range(0, 14):
+        c.drawString(350, ix*50+100, generateSubtractionQuestion(2, 10))
+
+    c.setFont("Helvetica", 10)
+    c.drawString(70, 70, "Genererad av MattePump 1.0" + "     " + str(date.today()))
+
+    c.save()
+
+generateQuestionSheet()
+#print (generateSubtractionQuestion(2, 10))
