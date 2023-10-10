1d630404aae6225bb8893287fe975d2db6ba7dd5
ecle20yy <ecle20yy@student.ju.se>
2023-10-10 20:06:57 +0200

    Change font to Courier
    This is to allow for commercial use

--- mattePump.py
+++ mattePump.py
@@ -42,19 +42,19 @@ def generateSubtractionQuestion(minSubtractedTerm: int, maxSubtractedTerm: int):
 def addInfoText(worksheet: canvas.Canvas, title: str):
     worksheet.setFillColor(colors.black)
 
-    worksheet.setFont("Helvetica", 30)
+    worksheet.setFont("Courier", 30)
     worksheet.drawString(70, 770, title)
 
-    worksheet.setFont("Helvetica", 15)
+    worksheet.setFont("Courier", 15)
     worksheet.drawString(70, 720, "Namn: ________________________________")
 
-    worksheet.setFont("Helvetica", 10)
+    worksheet.setFont("Courier", 10)
     worksheet.drawString(70, 70, "Genererad av MattePump 1.0")
     worksheet.drawString(70, 55, str(date.today()))
 
 def addQuestions(worksheet: canvas.Canvas):
     worksheet.setFillColor(colors.black)
-    worksheet.setFont("Helvetica", 24)
+    worksheet.setFont("Courier", 24)
     for ix in range(0, 12):
         worksheet.drawString(70, ix*50+120, generateAdditionQuestion(2, 10))
     for ix in range(0, 12):
@@ -69,4 +69,3 @@ def generateQuestionSheet():
     worksheet.save()
 
 generateQuestionSheet()
-#print (generateSubtractionQuestion(2, 10))
